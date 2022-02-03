7510d0a9e1dfb83b8e9489dbe059b3498f69766a
Matt Dudzik <mdudzik92@gmail.com>
2022-02-02 20:16:00 -0600

    Created the store, added actions and reducers

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+
 import Posts from './components/Posts';
 import PostForm from './components/Postform';
 
-function App() {
+import store from './store';
+
+class App extends Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <PostForm />
-      <hr />
-      <Posts />
-    </div>
-  );
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <PostForm />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
